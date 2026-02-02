eabfd11219cfa14a9730161ec2fc22a0096420af
Sajid Hussain <shussain@catapulto.ru>
2026-02-02 23:01:05 +0530

    test: return number for single input

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -4,4 +4,7 @@ describe('String Calculator', () => {
     it('returns 0 for empty string', () => {
         expect(add('')).toBe(0);
     });
+    it('returns number for single input', () => {
+        expect(add('1')).toBe(1);
+    });
 });
