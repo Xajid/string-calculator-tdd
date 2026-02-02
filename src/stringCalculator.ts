8c8affc32b5cadf42e4c1bf0022ef5f9b8872344
Sajid Hussain <shussain@catapulto.ru>
2026-02-02 23:25:52 +0530

    feat: suppors custom  delimiter

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,8 +1,18 @@
 export function add(numbers: string): number {
     if (numbers === '') return 0;
 
-    return numbers
-        .split(/,|\n/)
+    let delimiter = /,|\n/;
+    let numbersPart = numbers;
+
+    if (numbers.startsWith('//')) {
+        const parts = numbers.split('\n');
+        const customDelimiter = parts[0]?.substring(2) || ',';
+        delimiter = new RegExp(customDelimiter);
+        numbersPart = parts[1] || '';
+    }
+
+    return numbersPart
+        .split(delimiter)
         .map(Number)
         .reduce((sum, n) => sum + n, 0);
 }
