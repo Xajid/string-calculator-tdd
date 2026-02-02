ef4cd86c61e05c0ee5bb67bb7bb43878aa449c7c
Sajid Hussain <shussain@catapulto.ru>
2026-02-02 23:11:25 +0530

    test: handle new lines between numbers

--- test/stringCalculator.test.ts
+++ test/stringCalculator.test.ts
@@ -13,4 +13,7 @@ describe('String Calculator', () => {
     it('handles multiple numbers', () => {
         expect(add('1,2,3,4')).toBe(10);
     });
+    it('handles new lines between numbers', () => {
+        expect(add('1\n2,3')).toBe(6);
+    });
 });
