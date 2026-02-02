ad2f732f98920d1ceccc1ec4814c859cc0f50fed
Sajid Hussain <shussain@catapulto.ru>
2026-02-02 23:08:15 +0530

    feat: handle comma separated numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,8 @@
 export function add(numbers: string): number {
     if (numbers === '') return 0;
-    return parseInt(numbers, 10);
+
+    return numbers
+        .split(',')
+        .map(Number)
+        .reduce((sum, n) => sum + n, 0);
 }
